b7f12fb79ca773b99984ad72fab3ac28d6c4772b
5Amogh <amoghavarsh@navadhiti.com>
2022-08-23 17:38:40 +0530

    Angular migration to 11.0.0

--- projects/sl-questionnaire/src/lib/text-input/text-input.component.spec.ts
+++ projects/sl-questionnaire/src/lib/text-input/text-input.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TextInputComponent } from './text-input.component';
 
@@ -6,7 +6,7 @@ describe('TextInputComponent', () => {
   let component: TextInputComponent;
   let fixture: ComponentFixture<TextInputComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TextInputComponent ]
     })
