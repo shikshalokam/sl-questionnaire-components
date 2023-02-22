c7d92877641d87a166e01aea697fe8f9b52ef26e
5Amogh <amoghavarsh@navadhiti.com>
2023-02-22 13:49:53 +0530

    Angular version upgrade to version 14 from 13

--- projects/sl-questionnaire/src/lib/page-questions/page-questions.component.ts
+++ projects/sl-questionnaire/src/lib/page-questions/page-questions.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { UntypedFormGroup } from '@angular/forms';
 import { PageQuestion } from '../interfaces/questionnaire.type';
 
 @Component({
@@ -8,7 +8,7 @@ import { PageQuestion } from '../interfaces/questionnaire.type';
   styleUrls: ['./page-questions.component.css'],
 })
 export class PageQuestionsComponent implements OnInit {
-  @Input() questionnaireForm: FormGroup;
+  @Input() questionnaireForm: UntypedFormGroup;
   @Input() question: PageQuestion;
   constructor() {}
 
